4f92d8602dedba385ce93c0cdf724f9189867f4b
mateusw2705 <mateuswagner2705@gmail.com>
2024-02-19 14:41:09 -0300

    #

---
+++ WebApi/Model/Login.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi.Model
+{
+    public class Login
+    {
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string cpf { get; set; }
+  
+    }
+}
